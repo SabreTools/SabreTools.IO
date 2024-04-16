a69b6dfa3a13cef424905070b03169f3c675e889
Matt Nadareski <mnadareski@outlook.com>
2024-04-16 13:27:23 -0400

    Move composite stream to new namespace

--- SabreTools.IO/Streams/ReadOnlyCompositeStream.cs
+++ SabreTools.IO/Streams/ReadOnlyCompositeStream.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace SabreTools.IO
+namespace SabreTools.IO.Streams
 {
     /// <summary>
     /// Read-only stream wrapper around multiple, consecutive streams
