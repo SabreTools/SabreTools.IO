f3c54997543c5394a02c5753626d39d95f7c904c
Matt Nadareski <mnadareski@outlook.com>
2024-04-18 11:39:24 -0400

    Simplify method descriptions for BinaryReader extensions

--- SabreTools.IO/Extensions/BinaryReaderExtensions.cs
+++ SabreTools.IO/Extensions/BinaryReaderExtensions.cs
@@ -8,13 +8,8 @@ namespace SabreTools.IO.Extensions
     /// </summary>
     public static class BinaryReaderExtensions
     {
-        /// <summary>
-        /// Reads the specified number of bytes from the stream, starting from a specified point in the byte array.
-        /// </summary>
-        /// <param name="buffer">The buffer to read data into.</param>
-        /// <param name="index">The starting point in the buffer at which to begin reading into the buffer.</param>
-        /// <param name="count">The number of bytes to read.</param>
-        /// <returns>The number of bytes read into buffer. This might be less than the number of bytes requested if that many bytes are not available, or it might be zero if the end of the stream is reached.</returns>
+        /// <inheritdoc cref="BinaryReader.Read(byte[], int, int)"/>
+        /// <remarks>Reads in big-endian format</remarks>
         public static int ReadBigEndian(this BinaryReader reader, byte[] buffer, int index, int count)
         {
             int retval = reader.Read(buffer, index, count);
@@ -22,13 +17,8 @@ namespace SabreTools.IO.Extensions
             return retval;
         }
 
-        /// <summary>
-        /// Reads the specified number of characters from the stream, starting from a specified point in the character array.
-        /// </summary>
-        /// <param name="buffer">The buffer to read data into.</param>
-        /// <param name="index">The starting point in the buffer at which to begin reading into the buffer.</param>
-        /// <param name="count">The number of characters to read.</param>
-        /// <returns>The total number of characters read into the buffer. This might be less than the number of characters requested if that many characters are not currently available, or it might be zero if the end of the stream is reached.</returns>
+        /// <inheritdoc cref="BinaryReader.Read(char[], int, int)"/>
+        /// <remarks>Reads in big-endian format</remarks>
         public static int ReadBigEndian(this BinaryReader reader, char[] buffer, int index, int count)
         {
             int retval = reader.Read(buffer, index, count);
@@ -36,11 +26,8 @@ namespace SabreTools.IO.Extensions
             return retval;
         }
 
-        /// <summary>
-        /// Reads the specified number of bytes from the current stream into a byte array and advances the current position by that number of bytes.
-        /// </summary>
-        /// <param name="count">The number of bytes to read. This value must be 0 or a non-negative number or an exception will occur.</param>
-        /// <returns>A byte array containing data read from the underlying stream. This might be less than the number of bytes requested if the end of the stream is reached.</returns>
+        /// <inheritdoc cref="BinaryReader.ReadBytes(int)"/>
+        /// <remarks>Reads in big-endian format</remarks>
         public static byte[] ReadBytesBigEndian(this BinaryReader reader, int count)
         {
             byte[] retval = reader.ReadBytes(count);
@@ -48,11 +35,8 @@ namespace SabreTools.IO.Extensions
             return retval;
         }
 
-        /// <summary>
-        /// Reads the specified number of characters from the current stream, returns the data in a character array, and advances the current position in accordance with the Encoding used and the specific character being read from the stream.
-        /// </summary>
-        /// <param name="count">The number of characters to read. This value must be 0 or a non-negative number or an exception will occur.</param>
-        /// <returns>A character array containing data read from the underlying stream. This might be less than the number of bytes requested if the end of the stream is reached.</returns>
+        /// <inheritdoc cref="BinaryReader.ReadChars(int)"/>
+        /// <remarks>Reads in big-endian format</remarks>
         public static char[] ReadCharsBigEndian(this BinaryReader reader, int count)
         {
             char[] retval = reader.ReadChars(count);
@@ -60,10 +44,8 @@ namespace SabreTools.IO.Extensions
             return retval;
         }
 
-        /// <summary>
-        /// Reads a decimal value from the current stream and advances the current position of the stream by sixteen bytes.
-        /// </summary>
-        /// <returns>A decimal value read from the current stream.</returns>
+        /// <inheritdoc cref="BinaryReader.ReadDecimal"/>
+        /// <remarks>Reads in big-endian format</remarks>
         public static decimal ReadDecimalBigEndian(this BinaryReader reader)
         {
             byte[] retval = reader.ReadBytes(16);
@@ -77,10 +59,8 @@ namespace SabreTools.IO.Extensions
             return new decimal([i1, i2, i3, i4]);
         }
 
-        /// <summary>
-        /// eads an 8-byte floating point value from the current stream and advances the current position of the stream by eight bytes.
-        /// </summary>
-        /// <returns>An 8-byte floating point value read from the current stream.</returns>
+        /// <inheritdoc cref="BinaryReader.ReadDouble"/>
+        /// <remarks>Reads in big-endian format</remarks>
         public static double ReadDoubleBigEndian(this BinaryReader reader)
         {
             byte[] retval = reader.ReadBytes(8);
@@ -88,10 +68,8 @@ namespace SabreTools.IO.Extensions
             return BitConverter.ToDouble(retval, 0);
         }
 
-        /// <summary>
-        /// Reads a 2-byte signed integer from the current stream and advances the current position of the stream by two bytes.
-        /// </summary>
-        /// <returns>A 2-byte signed integer read from the current stream.</returns>
+        /// <inheritdoc cref="BinaryReader.ReadInt16"/>
+        /// <remarks>Reads in big-endian format</remarks>
         public static short ReadInt16BigEndian(this BinaryReader reader)
         {
             byte[] retval = reader.ReadBytes(2);
@@ -99,10 +77,8 @@ namespace SabreTools.IO.Extensions
             return BitConverter.ToInt16(retval, 0);
         }
 
-        /// <summary>
-        /// Reads a 4-byte signed integer from the current stream and advances the current position of the stream by four bytes.
-        /// </summary>
-        /// <returns>A 4-byte signed integer read from the current stream.</returns>
+        /// <inheritdoc cref="BinaryReader.ReadInt32"/>
+        /// <remarks>Reads in big-endian format</remarks>
         public static int ReadInt32BigEndian(this BinaryReader reader)
         {
             byte[] retval = reader.ReadBytes(4);
@@ -110,10 +86,8 @@ namespace SabreTools.IO.Extensions
             return BitConverter.ToInt32(retval, 0);
         }
 
-        /// <summary>
-        /// Reads an 8-byte signed integer from the current stream and advances the current position of the stream by eight bytes.
-        /// </summary>
-        /// <returns>An 8-byte signed integer read from the current stream.</returns>
+        /// <inheritdoc cref="BinaryReader.ReadInt64"/>
+        /// <remarks>Reads in big-endian format</remarks>
         public static long ReadInt64BigEndian(this BinaryReader reader)
         {
             byte[] retval = reader.ReadBytes(8);
@@ -121,10 +95,8 @@ namespace SabreTools.IO.Extensions
             return BitConverter.ToInt64(retval, 0);
         }
 
-        /// <summary>
-        /// Reads a 4-byte floating point value from the current stream and advances the current position of the stream by four bytes.
-        /// </summary>
-        /// <returns>A 4-byte floating point value read from the current stream.</returns>
+        /// <inheritdoc cref="BinaryReader.ReadSingle"/>
+        /// <remarks>Reads in big-endian format</remarks>
         public static float ReadSingleBigEndian(this BinaryReader reader)
         {
             byte[] retval = reader.ReadBytes(4);
@@ -132,12 +104,8 @@ namespace SabreTools.IO.Extensions
             return BitConverter.ToSingle(retval, 0);
         }
 
-        /// <summary>
-        /// Reads a 2-byte unsigned integer from the current stream using little-endian encoding and advances the position of the stream by two bytes.
-        /// 
-        /// This API is not CLS-compliant.
-        /// </summary>
-        /// <returns>A 2-byte unsigned integer read from this stream.</returns>
+        /// <inheritdoc cref="BinaryReader.ReadUInt16"/>
+        /// <remarks>Reads in big-endian format</remarks>
         public static ushort ReadUInt16BigEndian(this BinaryReader reader)
         {
             byte[] retval = reader.ReadBytes(2);
@@ -145,12 +113,7 @@ namespace SabreTools.IO.Extensions
             return BitConverter.ToUInt16(retval, 0);
         }
 
-        /// <summary>
-        /// Reads a 4-byte unsigned integer from the current stream and advances the position of the stream by four bytes.
-        /// 
-        /// This API is not CLS-compliant.
-        /// </summary>
-        /// <returns>A 4-byte unsigned integer read from this stream.</returns>
+        /// <remarks>Reads in big-endian format</remarks>
         public static uint ReadUInt32BigEndian(this BinaryReader reader)
         {
             byte[] retval = reader.ReadBytes(4);
@@ -158,12 +121,8 @@ namespace SabreTools.IO.Extensions
             return BitConverter.ToUInt32(retval, 0);
         }
 
-        /// <summary>
-        /// Reads an 8-byte unsigned integer from the current stream and advances the position of the stream by eight bytes.
-        /// 
-        /// This API is not CLS-compliant.
-        /// </summary>
-        /// <returns>An 8-byte unsigned integer read from this stream.</returns>
+        /// <inheritdoc cref="BinaryReader.ReadUInt64"/>
+        /// <remarks>Reads in big-endian format</remarks>
         public static ulong ReadUInt64BigEndian(this BinaryReader reader)
         {
             byte[] retval = reader.ReadBytes(8);
