0b7ab5b932d08e517fdbd14339ab4099a751a398
Matt Nadareski <mnadareski@outlook.com>
2024-11-25 22:44:40 -0500

    Add more Bitstream tests

--- SabreTools.IO.Test/Streams/ReadOnlyBitStreamTests.cs
+++ SabreTools.IO.Test/Streams/ReadOnlyBitStreamTests.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Linq;
 using SabreTools.IO.Streams;
 using Xunit;
 
@@ -56,6 +57,63 @@ namespace SabreTools.IO.Test.Streams
             Assert.NotNull(actual);
             Assert.Equal(expected, actual);
             Assert.Equal(position, stream.Position);
+        }
+
+        [Fact]
+        public void ReadByteTest()
+        {
+            byte[] data = [0b01010101, 0b01010101, 0b01010101, 0b01010101];
+            byte expected = 0b01010101;
+
+            var stream = new ReadOnlyBitStream(new MemoryStream(data));
+            byte? actual = stream.ReadByte();
+            Assert.NotNull(actual);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReadUInt16Test()
+        {
+            byte[] data = [0b01010101, 0b01010101, 0b01010101, 0b01010101];
+            ushort expected = 0b0101010101010101;
+
+            var stream = new ReadOnlyBitStream(new MemoryStream(data));
+            ushort? actual = stream.ReadUInt16();
+            Assert.NotNull(actual);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReadUInt32Test()
+        {
+            byte[] data = [0b01010101, 0b01010101, 0b01010101, 0b01010101];
+            uint expected = 0b01010101010101010101010101010101;
+
+            var stream = new ReadOnlyBitStream(new MemoryStream(data));
+            uint? actual = stream.ReadUInt32();
+            Assert.NotNull(actual);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReadUInt64Test()
+        {
+            byte[] data = [0b01010101, 0b01010101, 0b01010101, 0b01010101];
+
+            var stream = new ReadOnlyBitStream(new MemoryStream(data));
+            ulong? actual = stream.ReadUInt64();
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public void ReadBytesTest()
+        {
+            byte[] data = [0b01010101, 0b01010101, 0b01010101, 0b01010101];
+
+            var stream = new ReadOnlyBitStream(new MemoryStream(data));
+            byte[]? actual = stream.ReadBytes(4);
+            Assert.NotNull(actual);
+            Assert.True(data.SequenceEqual(actual));
         }
     }
 }
