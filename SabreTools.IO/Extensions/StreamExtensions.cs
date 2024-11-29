d2d191d86f444e0f1224ec442ca74f58157a59cf
Matt Nadareski <mnadareski@outlook.com>
2024-11-28 21:17:33 -0500

    Add boundary alignment stream extension

--- SabreTools.IO/Extensions/StreamExtensions.cs
+++ SabreTools.IO/Extensions/StreamExtensions.cs
@@ -5,6 +5,32 @@ namespace SabreTools.IO.Extensions
     public static class StreamExtensions
     {
         /// <summary>
+        /// Align the stream position to a byte-size boundary
+        /// </summary>
+        /// <param name="input">Input stream to try aligning</param>
+        /// <param name="alignment">Number of bytes to align on</param>
+        /// <returns>True if the stream could be aligned, false otherwise</returns>
+        public static bool AlignToBoundary(this Stream input, byte alignment)
+        {
+            // If the stream is invalid
+            if (input == null || input.Length == 0 || !input.CanRead)
+                return false;
+
+            // If already at the end of the stream
+            if (input.Position >= input.Length)
+                return false;
+
+            // Align the stream position
+            while (input.Position % alignment != 0 && input.Position < input.Length)
+            {
+                _ = input.ReadByteValue();
+            }
+
+            // Return if the alignment completed
+            return input.Position % alignment == 0;
+        }
+
+        /// <summary>
         /// Seek to a specific point in the stream, if possible
         /// </summary>
         /// <param name="input">Input stream to try seeking on</param>
