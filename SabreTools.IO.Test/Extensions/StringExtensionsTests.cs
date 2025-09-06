b22384d5f3ed326de2ddf495306068f86290fb50
Matt Nadareski <mnadareski@outlook.com>
2025-09-06 15:32:36 -0400

    Add neat string extensions from BOS

---
+++ SabreTools.IO.Test/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,59 @@
+using SabreTools.IO.Extensions;
+using Xunit;
+
+namespace SabreTools.IO.Test.Extensions
+{
+    public class StringExtensionsTests
+    {
+        #region OptionalContains
+
+        [Theory]
+        [InlineData(null, "ANY", false)]
+        [InlineData("", "ANY", false)]
+        [InlineData("ANY", "ANY", true)]
+        [InlineData("ANYTHING", "ANY", true)]
+        [InlineData("THING", "ANY", false)]
+        [InlineData("THINGANY", "ANY", true)]
+        public void OptionalContainsTest(string? haystack, string needle, bool expected)
+        {
+            bool actual = haystack.OptionalContains(needle);
+            Assert.Equal(expected, actual);
+        }
+
+        #endregion
+
+        #region OptionalEquals
+
+        [Theory]
+        [InlineData(null, "ANY", false)]
+        [InlineData("", "ANY", false)]
+        [InlineData("ANY", "ANY", true)]
+        [InlineData("ANYTHING", "ANY", false)]
+        [InlineData("THING", "ANY", false)]
+        [InlineData("THINGANY", "ANY", false)]
+        public void OptionalEqualsTest(string? haystack, string needle, bool expected)
+        {
+            bool actual = haystack.OptionalEquals(needle);
+            Assert.Equal(expected, actual);
+        }
+
+        #endregion
+
+        #region OptionalStartsWith
+
+        [Theory]
+        [InlineData(null, "ANY", false)]
+        [InlineData("", "ANY", false)]
+        [InlineData("ANY", "ANY", true)]
+        [InlineData("ANYTHING", "ANY", true)]
+        [InlineData("THING", "ANY", false)]
+        [InlineData("THINGANY", "ANY", false)]
+        public void OptionalStartsWithTest(string? haystack, string needle, bool expected)
+        {
+            bool actual = haystack.OptionalStartsWith(needle);
+            Assert.Equal(expected, actual);
+        }
+
+        #endregion
+    }
+}
---
+++ SabreTools.IO/Extensions/StringExtensions.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace SabreTools.IO.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <inheritdoc cref="string.Contains(string)"/>
+        public static bool OptionalContains(this string? self, string value)
+            => OptionalContains(self, value, StringComparison.Ordinal);
+
+        /// <inheritdoc cref="string.Contains(string, StringComparison)"/>
+        public static bool OptionalContains(this string? self, string value, StringComparison comparisonType)
+        {
+            if (self == null)
+                return false;
+
+#if NETFRAMEWORK
+            return self.Contains(value);
+#else
+            return self.Contains(value, comparisonType);
+#endif
+        }
+
+        /// <inheritdoc cref="string.Equals(string)"/>
+        public static bool OptionalEquals(this string? self, string value)
+            => OptionalEquals(self, value, StringComparison.Ordinal);
+
+        /// <inheritdoc cref="string.Equals(string, StringComparison)"/>
+        public static bool OptionalEquals(this string? self, string value, StringComparison comparisonType)
+        {
+            if (self == null)
+                return false;
+
+            return self.Equals(value, comparisonType);
+        }
+
+        /// <inheritdoc cref="string.StartsWith(string)"/>
+        public static bool OptionalStartsWith(this string? self, string value)
+            => OptionalStartsWith(self, value, StringComparison.Ordinal);
+
+        /// <inheritdoc cref="string.StartsWith(string, StringComparison)"/>
+        public static bool OptionalStartsWith(this string? self, string value, StringComparison comparisonType)
+        {
+            if (self == null)
+                return false;
+
+            return self.StartsWith(value, comparisonType);
+        }
+    }
+}
