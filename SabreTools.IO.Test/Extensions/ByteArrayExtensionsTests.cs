bea48829f4868b990c9ee69d689f71ad5ead6108
Matt Nadareski <mnadareski@outlook.com>
2026-02-12 13:48:52 -0500

    Add byte array alignment extension from Serialization

--- SabreTools.IO.Test/Extensions/ByteArrayExtensionsTests.cs
+++ SabreTools.IO.Test/Extensions/ByteArrayExtensionsTests.cs
@@ -8,6 +8,65 @@ namespace SabreTools.IO.Test.Extensions
 {
     public class ByteArrayExtensionsTests
     {
+        #region AlignToBoundary
+
+        [Fact]
+        public void AlignToBoundary_Null_False()
+        {
+            byte[]? arr = null;
+            int offset = 0;
+            byte alignment = 4;
+
+            bool actual = arr.AlignToBoundary(ref offset, alignment);
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void AlignToBoundary_Empty_False()
+        {
+            byte[]? arr = [];
+            int offset = 0;
+            byte alignment = 4;
+
+            bool actual = arr.AlignToBoundary(ref offset, alignment);
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void AlignToBoundary_EOF_False()
+        {
+            byte[]? arr = [0x01, 0x02];
+            int offset = 1;
+            byte alignment = 4;
+
+            bool actual = arr.AlignToBoundary(ref offset, alignment);
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void AlignToBoundary_TooShort_False()
+        {
+            byte[]? arr = [0x01, 0x02];
+            int offset = 1;
+            byte alignment = 4;
+
+            bool actual = arr.AlignToBoundary(ref offset, alignment);
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void AlignToBoundary_CanAlign_True()
+        {
+            byte[]? arr = [0x01, 0x02, 0x03, 0x04, 0x05];
+            int offset = 1;
+            byte alignment = 4;
+
+            bool actual = arr.AlignToBoundary(ref offset, alignment);
+            Assert.True(actual);
+        }
+
+        #endregion
+
         #region IsNullOrEmpty
 
         [Fact]
--- SabreTools.IO/Extensions/ByteArrayExtensions.cs
+++ SabreTools.IO/Extensions/ByteArrayExtensions.cs
@@ -9,6 +9,33 @@ namespace SabreTools.IO.Extensions
     public static class ByteArrayExtensions
     {
         /// <summary>
+        /// Align the array position to a byte-size boundary
+        /// </summary>
+        /// <param name="input">Input array to try aligning</param>
+        /// <param name="offset">Offset into the byte array</param>
+        /// <param name="alignment">Number of bytes to align on</param>
+        /// <returns>True if the array could be aligned, false otherwise</returns>
+        public static bool AlignToBoundary(this byte[]? input, ref int offset, byte alignment)
+        {
+            // If the array is invalid
+            if (input is null || input.Length == 0)
+                return false;
+
+            // If already at the end of the array
+            if (offset >= input.Length)
+                return false;
+
+            // Align the stream position
+            while (offset % alignment != 0 && offset < input.Length)
+            {
+                _ = input.ReadByteValue(ref offset);
+            }
+
+            // Return if the alignment completed
+            return offset % alignment == 0;
+        }
+
+        /// <summary>
         /// Indicates whether the specified array is null or has a length of zero
         /// </summary>
         public static bool IsNullOrEmpty(this Array? array)
