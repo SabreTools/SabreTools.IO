c21478ed426e2ee49c529c5817caf23189b3c03e
Matt Nadareski <mnadareski@outlook.com>
2026-03-18 17:19:59 -0400

    Backport extensions from ST

---
+++ SabreTools.IO.Test/Extensions/DateTimeExtensionsTests.cs
@@ -0,0 +1,65 @@
+using System;
+using SabreTools.Core.Tools;
+using Xunit;
+
+namespace SabreTools.Core.Test.Tools
+{
+    public class DateTimeExtensionsTests
+    {
+        #region ConvertToMsDosTimeFormat
+
+        [Fact]
+        public void ConvertToMsDosTimeFormat_MinSupported()
+        {
+            long expected = 2162688;
+            DateTime dateTime = new DateTime(1980, 01, 01, 00, 00, 00, 00);
+            long actual = dateTime.ConvertToMsDosTimeFormat();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ConvertToMsDosTimeFormat_Y2K()
+        {
+            long expected = 673251328;
+            DateTime dateTime = new DateTime(2000, 01, 01, 00, 00, 00, 00);
+            long actual = dateTime.ConvertToMsDosTimeFormat();
+            Assert.Equal(expected, actual);
+        }
+
+        #endregion
+
+        #region ConvertFromMsDosTimeFormat
+
+        [Fact]
+        public void ConvertFromMsDosTimeFormat_MinSupported()
+        {
+            uint msDosDateTime = 2162688;
+            DateTime actual = msDosDateTime.ConvertFromMsDosTimeFormat();
+
+            Assert.Equal(1980, actual.Year);
+            Assert.Equal(01, actual.Month);
+            Assert.Equal(01, actual.Day);
+            Assert.Equal(00, actual.Hour);
+            Assert.Equal(00, actual.Minute);
+            Assert.Equal(00, actual.Second);
+            Assert.Equal(000, actual.Millisecond);
+        }
+
+        [Fact]
+        public void ConvertFromMsDosTimeFormat_Y2K()
+        {
+            uint msDosDateTime = 673251328;
+            DateTime actual = msDosDateTime.ConvertFromMsDosTimeFormat();
+
+            Assert.Equal(2000, actual.Year);
+            Assert.Equal(01, actual.Month);
+            Assert.Equal(01, actual.Day);
+            Assert.Equal(00, actual.Hour);
+            Assert.Equal(00, actual.Minute);
+            Assert.Equal(00, actual.Second);
+            Assert.Equal(000, actual.Millisecond);
+        }
+
+        #endregion
+    }
+}
---
+++ SabreTools.IO/SabreTools.IO.Extensions/DateTimeExtensions.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace SabreTools.Core.Tools
+{
+    public static class DateTimeExtensions
+    {
+        // <summary>
+        /// Convert .NET DateTime to MS-DOS date format
+        /// </summary>
+        /// <remarks>Adapted from 7-zip Source Code: CPP/Windows/TimeUtils.cpp:FileTimeToDosTime</remarks>
+        public static long ConvertToMsDosTimeFormat(this DateTime dateTime)
+        {
+            uint year = (uint)((dateTime.Year - 1980) % 128);
+            uint mon = (uint)dateTime.Month;
+            uint day = (uint)dateTime.Day;
+            uint hour = (uint)dateTime.Hour;
+            uint min = (uint)dateTime.Minute;
+            uint sec = (uint)dateTime.Second;
+
+            return (year << 25) | (mon << 21) | (day << 16) | (hour << 11) | (min << 5) | (sec >> 1);
+        }
+
+        /// <summary>
+        /// Convert MS-DOS date format to .NET DateTime
+        /// </summary>
+        /// <remarks>Adapted from 7-zip Source Code: CPP/Windows/TimeUtils.cpp:DosTimeToFileTime</remarks>
+        public static DateTime ConvertFromMsDosTimeFormat(this uint msDosDateTime)
+        {
+            return new DateTime((int)(1980 + (msDosDateTime >> 25)), (int)((msDosDateTime >> 21) & 0xF), (int)((msDosDateTime >> 16) & 0x1F),
+                (int)((msDosDateTime >> 11) & 0x1F), (int)((msDosDateTime >> 5) & 0x3F), (int)((msDosDateTime & 0x1F) * 2));
+        }
+    }
+}
