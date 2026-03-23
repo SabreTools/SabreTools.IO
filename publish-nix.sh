6302cf36db080a77bf5573e2e371b196b88d539b
Matt Nadareski <mnadareski@outlook.com>
2026-03-23 11:39:46 -0400

    Add unpublished packages to publish scripts

--- publish-nix.sh
+++ publish-nix.sh
@@ -31,6 +31,22 @@ then
     echo "Restoring Nuget packages"
     dotnet restore
 
-    # Create Nuget Package
+    # Create published Nuget Package
     dotnet pack SabreTools.IO.Meta/SabreTools.IO.Meta.csproj --output $BUILD_FOLDER
+
+    # Create unpublished Nuget Packages
+    dotnet pack SabreTools.Collections.Extensions/SabreTools.Collections.Extensions.csproj --output $BUILD_FOLDER
+    dotnet pack SabreTools.IO/SabreTools.IO.csproj --output $BUILD_FOLDER
+    dotnet pack SabreTools.IO.Compression/SabreTools.IO.Compression.csproj --output $BUILD_FOLDER
+    dotnet pack SabreTools.IO.Extensions/SabreTools.IO.Extensions.csproj --output $BUILD_FOLDER
+    dotnet pack SabreTools.Logging/SabreTools.Logging.csproj --output $BUILD_FOLDER
+    dotnet pack SabreTools.Matching/SabreTools.Matching.csproj --output $BUILD_FOLDER
+    dotnet pack SabreTools.Numerics/SabreTools.Numerics.csproj --output $BUILD_FOLDER
+    dotnet pack SabreTools.Numerics.Extensions/SabreTools.Numerics.Extensions.csproj --output $BUILD_FOLDER
+    dotnet pack SabreTools.Security.Cryptography/SabreTools.Security.Cryptography.csproj --output $BUILD_FOLDER
+    dotnet pack SabreTools.Text.ClrMamePro/SabreTools.Text.ClrMamePro.csproj --output $BUILD_FOLDER
+    dotnet pack SabreTools.Text.Compare/SabreTools.Text.Compare.csproj --output $BUILD_FOLDER
+    dotnet pack SabreTools.Text.Extensions/SabreTools.Text.Extensions.csproj --output $BUILD_FOLDER
+    dotnet pack SabreTools.Text.INI/SabreTools.Text.INI.csproj --output $BUILD_FOLDER
+    dotnet pack SabreTools.Text.SeparatedValue/SabreTools.Text.SeparatedValue.csproj --output $BUILD_FOLDER
 fi
